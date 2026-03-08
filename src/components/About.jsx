0987a7d54f91616d8a7a1b4e046ded2ec9829e7d
Sanjaypandi123 <sanjayelanchikani@gmail.com>
2026-03-08 22:42:05 +0530

    project-on-load

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import Header from './Header'
 import '../Csss/About.css'
-import { Link } from 'react-router-dom'
+import cv from '../assets/Sanjay Resume.pdf'
 
 const About = () => {
   return (
     <>
     <Header/>
-    <div className="container-fluid wrapper">
+    <div className="container-fluid wrapper py-5">
           <div className="img">
 
           </div>
@@ -16,29 +16,26 @@ const About = () => {
           </div>
 
           <div className="heading">
-            <h1>RESUME <h2>ABOUT <span>ME</span></h2></h1>
+            <h1>RESUME <p>ABOUT <span>ME</span></p></h1>
             
           </div>
 
           <div className="about-wrapper mt-5">
-            <div className="img1 d-lg-none mb-3">
+            <div className="img1 d-lg-none mb-5">
 
-          </div>
+            </div>
           
             <h1>PERSONAL INFOS</h1>
               <div className="row py-4">
-                <div className="col-lg-6 lft ">
-                  <h5>First Name : <span>Sanjay</span></h5>
-                  <h5>Phone : <span>+91-8220316161</span></h5>
-                  <h5>Age : <span>20</span></h5>
-                </div>
-                <div className="col-lg-6 rit">
-                  <h5>Last Name : <span>PandiDurai</span></h5>
-                  <h5>Nationality : <span>INDIAN</span></h5>
-                  <h5>Address : <span>Trichy </span></h5>
+                <div className="col-lg-12 ">
+                  <h4>Name : <span>Sanjay PandiDurai</span></h4>
+                  <h4>Phone : <span>+91-8220316161</span></h4>
+                  <h4>Age : <span>20</span></h4>
+                  <h4>Nationality : <span>INDIAN</span></h4>
+                  <h4>Address : <span>Trichy </span></h4>
                 </div>
               </div>
-              <h1>Hobbies</h1>
+              {/* <h1>Hobbies</h1>
               <div className="row py-4">
                 <div className="col-lg-4 col-md-4  hobis">
                   <h5 className='text-center'><span><i className="fa-solid fa-motorcycle"></i></span>Travals</h5>
@@ -49,14 +46,41 @@ const About = () => {
                 <div className="col-lg-4 col-md-4  hobis">
                   <h5 className='text-center'><span><i className="fa-solid fa-gamepad"></i></span>Travals</h5>
                 </div>
-              </div>
+              </div> */}
               <div className="BTN pt-3">
-                <Link className='button'><span><b>DOWNLOAD CV</b></span> <p><i className="fa-solid fa-download"></i></p></Link>
+                <a href={cv} download className='button'><span><b>DOWNLOAD CV</b></span> <p><i className="fa-solid fa-download"></i></p></a>
               </div>
-              {
-                
-              }
           </div>
+      </div>
+      <div className="container-fluid wrapper py-5 d-flex align-items-center justify-content-center ">
+             <div className="row">
+<div className="col-lg-12">
+<div className="timeline-item">
+<div className="icon"><i className="fa-solid fa-briefcase"></i></div>
+<span className="badge mb-2">2019 - 2020 </span>
+<h5 className=''>Secondary School (SSLC)  <span className="">–   Samayapuram</span></h5>
+<p>Government High School S.Kannanur.</p>
+<p>71%.</p>
+</div>
+
+<div className="timeline-item">
+<div className="icon"><i className="fa-solid fa-briefcase"></i></div>
+<span className="badge mb-2">2020 - 2022</span>
+<h5 className=''>Higher Secondary (HSC) <span className="">– Mannachanallur</span></h5>
+<p>Government Boys Hr Sec School Mannachanallur.</p>
+<p>46%.</p>
+</div>
+
+<div className="timeline-item">
+<div className="icon"><i className="fa-solid fa-briefcase"></i></div>
+<span className="badge mb-2">2022–2025 </span>
+<h5 className=''>BACHELOR DEGREE  <span className="">– Bharathidasan University</span></h5>
+<p>SRM TRICHY ARTS AND SCIENCE COLLEGE, Irungalur.</p>
+<p>60%.</p>
+</div>
+
+</div> 
+</div>
       </div>
     </>
   )
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,12 +2,22 @@ import React from 'react'
 import Header from './Header'
 import '../Csss/Home.css'
 import { Link } from "react-router-dom"
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+  
+    const fun=useNavigate()
+
+  let gotoabout =()=>{
+       fun('/about')
+   }
+  
   return (
+    
     <>
+    
     <Header/>
-      <div className="container-fluid wrapper home-wrapper">
+      <div className="container-fluid wrapper home-wrapper sideimg">
           <div className="img">
 
           </div>
@@ -24,10 +34,14 @@ const Home = () => {
               <h2>MERN STACK DEVELOPER</h2>
               <h6 className='pt-3'>I'm from Trichy. I have done my degree in computer science <br /> at SRM Trichy arts and science college. I have experience <br /> in web development. I'm eager to learn and grow in a challenging<br /> environment.That's all about me....</h6>
               <div className="BTN pt-3">
-                <Link className='button' to="/about"><span><b>MORE ABOUT ME</b></span> <p><i className="fa-solid fa-arrow-right"></i></p></Link>
+                {/* <Link className='button' to="/about"><span><b>MORE ABOUT ME</b></span> <p><i className="fa-solid fa-arrow-right"></i></p></Link> */}
+                <button className='button' onClick={gotoabout} ><span><b>MORE ABOUT MEe</b></span> <p><i className="fa-solid fa-arrow-right"></i></p></button>
               </div>
           </div>
       </div>
+
+
+
     </>
   )
 }
